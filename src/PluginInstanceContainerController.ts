851fbad170808db7484018013a413eb7700a101e
Faiz A. Farooqui <faizz.af@gmail.com>
2023-02-01 13:47:02 +0530

    chore: remove unused up and down method

--- src/PluginInstanceContainerController.ts
+++ src/PluginInstanceContainerController.ts
@@ -120,96 +120,11 @@ export class PluginInstanceContainerController implements IContainerController {
   getConfig(): any { }
 
   async up() {
-    return;
-    if (this.getStatus() !== "up") {
-      if (!this.callerInstance.getMinioInstance()) {
-        throw new Error(
-          `No minio instance attached with ${this.callerInstance.getName()}`,
-        );
-      }
-      if (!this.callerInstance.getMinioInstance()?.getContainerController()) {
-        throw new Error(
-          `Not a valid minio storage configured with ${this.callerInstance.getName()}`,
-        );
-      }
-      if (
-        this.callerInstance
-          .getMinioInstance()
-          ?.getContainerController()
-          ?.getStatus() !== "up"
-      ) {
-        await this.callerInstance
-          .getMinioInstance()
-          ?.getContainerController()
-          ?.up();
-      }
-
-      await new Promise(async (resolve, reject) => {
-        console.log("\x1b[33m");
-        console.log(
-          `${this.callerInstance.getName()}: Running "${this.installScript().join(
-            " ",
-          )}"`,
-          "\x1b[0m",
-        );
-        SpawnHelper.run(
-          this.callerInstance.getInstallationPath(),
-          this.installScript(),
-        )
-          .then(() => {
-            console.log("\x1b[33m");
-            console.log(
-              `${this.callerInstance.getName()}: Running "${this.runScript().join(
-                " ",
-              )}"`,
-              "\x1b[0m",
-            );
-            SpawnHelper.start(
-              this.callerInstance.getInstallationPath(),
-              this.runScript(),
-            )
-              .then(async ({ processId }: { processId: string }) => {
-                this.setStatus("up");
-                this.setContainerId(processId);
-                console.log("\x1b[32m");
-                console.log(
-                  `Use http://localhost:${await this.getPortNumber()}/upload as your storage endpoint`,
-                );
-                console.log("\x1b[0m");
-                return resolve(true);
-              })
-              .catch((e: any) => {
-                return reject(e);
-              });
-          })
-          .catch((e: any) => {
-            return reject(e);
-          });
-      });
-    } else {
-      console.log("\x1b[32m");
-      console.log(
-        `Use http://localhost:${await this.getPortNumber()}/upload as your storage endpoint`,
-      );
-      console.log("\x1b[0m");
-    }
+    //
   }
 
   async down() {
-    return;
-    if (this.getStatus() !== "down") {
-      await new Promise(async (resolve, reject) => {
-        SpawnHelper.stop(this.getContainerId(), this.callerInstance.getName())
-          .then(() => {
-            this.setStatus("down");
-            this.setContainerId(null);
-            return resolve(true);
-          })
-          .catch((e: any) => {
-            return reject(e);
-          });
-      });
-    }
+    //
   }
 
   async build() {
